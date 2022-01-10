f551ed0fd725f9599d666bc4755c7a25dd353a90
unknown <hindhujhasridhar@gmail.com>
2022-01-10 11:03:46 +0530

    TABLE IS CREATED

--- AddressBook/AddressBook/SQLQuery1.sql
+++ AddressBook/AddressBook/SQLQuery1.sql
@@ -2,3 +2,15 @@ create database Address_Book_Service
 
 use Address_Book_Service
 
+create table AddressBook(
+FirstName varchar(100),
+LastName varchar(100),
+Address varchar(255),
+City varchar(50),
+state varchar(50),
+Zip int,
+PhoneNumber varchar(100),
+email varchar(100)
+)
+
+select * from AddressBook 
